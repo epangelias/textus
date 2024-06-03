12f42b36cdb23cf8889b3d7d6c5db9aecf6c5a8f
Epangelias <epangealis@gmail.com>
2024-06-02 22:19:16 -0700

    translucent top bar iphone

--- components/WebAppify.tsx
+++ components/WebAppify.tsx
@@ -2,6 +2,10 @@ export function WebAppify() {
   return (
     <>
       {/* <meta name='theme-color' content='#FFFFFF' /> */}
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="translucent"
+      />
       <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="msapplication-tap-highlight" content="no" />
